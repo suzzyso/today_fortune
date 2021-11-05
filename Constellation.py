7654dc90411d228fb6a13d00ef10ee63d380c6da
이정현 <skyhamasaki1@gmail.com>
2021-11-05 21:46:45 +0900

    중간최종본

--- Constellation.py
+++ Constellation.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient     # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
 #client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.Constellation                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
